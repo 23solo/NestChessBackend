14c40aa0f9b6ff9393364b5740fe429b38fab10f
23solo <yagami.naruto23@gmail.com>
2023-09-06 13:47:49 +0530

    [TESt] added log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
+    console.log(
+      'Domain is',
+      this.configService.get('DOMAIN'),
+    );
     return 'Hello World!';
   }
 }
